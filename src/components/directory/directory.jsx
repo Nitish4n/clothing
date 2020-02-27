f3453fd62ebe1109e69e39953919b1f0f6c4a3a9
nitish4n@gmail.com <nitish4n@gmail.com>
2020-02-28 01:36:45 +0530

    state issue resolved

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -32,7 +32,6 @@ class directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  size: 'large',
                   linkUrl: 'shop/womens'
                 },
                 {
@@ -40,7 +39,6 @@ class directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  size: 'large',
                   linkUrl: 'shop/mens'
                 }
               ]
