62d292cb155bd374288c731d2f3c982d51c210de
nitish4n@gmail.com <nitish4n@gmail.com>
2020-03-29 13:50:28 +0530

    Component into smaller pieces and hover effects: Section 4 completed

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './menu-item.scss';
 
 const MenuItem = ({ title, imageURL , size}) => (
-    <div style={{backgroundImage:`url(${imageURL})`}} className={`${size} menu-item`} >
+    <div className={`${size} menu-item`} >
+        <div className="background-image" style={{backgroundImage:`url(${imageURL})`}} >
+
+        </div>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
